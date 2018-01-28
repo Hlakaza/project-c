33b85dba2edea78748b185328e7e85df9a0f4a38
Lucky <nhlalucky9@gmail.com>
2018-01-28 21:30:37 +0200

    refactor(user): added user inputs

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,31 @@
-﻿export class User {
+﻿import { EmailValidator } from "@angular/forms/src/directives/validators";
+
+export class User {
     id: number;
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
+    username: string; // Email
+    password: string; //
+    firstName: string; // Name of the person
+    lastName: string;  // surname of person
+    tradingName: string; // Trading Name of company
+    registeredCompanyName: string;
+    registrationNumber: string;
+    physicalAddress: string;
+    postalAddress: string;
+    tel: string; // business tel
+    faxNo: string;
+    website: string;
+    vatNumber: string;
+    accPersonName: string;
+    accPersonNo: string;
+    accPersonFaxNo: string;
+    accPersonEmail: string;
+    salesPersonName: string;
+    salesPersonNo: string;
+    salesPersonEmail: string;
+    salesPersonFaxNo: string;
+    termsOfSupply: File;
+
+
+
+
 }
