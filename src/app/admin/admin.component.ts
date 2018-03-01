de7cdac1002d7f1cfd78db4c7abb66ae4c9a01f1
Lucky Hlakaza <nhlalucky9@gmail.com>
2018-03-01 08:33:39 +0200

    refactor(admin): complete admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+  logout() {
+    return this.authService.logout();
   }
 
 }
