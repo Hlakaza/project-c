b9caf3629102801883c15e154067d64b8215f406
Lucky Hlakaza <nhlalucky9@gmail.com>
2018-05-14 13:11:38 +0200

    refactor(server): updated the resourses

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    'database': 'mongodb://luckyH:Tempest16**@ds237868.mlab.com:37868/clinix',
-    // 'database': 'mongodb://localhost:27017/clinix',
+    'database': 'mongodb://localhost:27017/clinix',
     'secret': 'SUPERsecretisSuperDaper', // change this to a hard to guess random string. it's for jwt encryption and decryption
-    'api_user': 'luckyH',
-    'api_key': 'Maboe62263**',
+    'api_user': 'your_username',
+    'api_key': 'your_api_key',
     'jwtExpire': '5h', //set the jwtExpire in smaller period in production
     'paths': {
         // path for pets images
